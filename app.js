9d467b8ddce69e0e1deea673d1308a67f952a3d0
Rygno <alfred@rygno.net>
2022-01-16 00:18:33 +0000

    add delay feature

--- app.js
+++ app.js
@@ -18,8 +18,18 @@ app.listen(config.port, config.hostname, () => {
 	console.log("started server on port %d", config.port)
 })
 
-app.post('/setPowerState/:enabled', function (req, res) {
+app.post('/setPowerState/:enabled/:delay?', function (req, res) {
 	newState = req.params.enabled == 'on' ? 'on' : 'off'
-	ewlConnect().setDevicePowerState(config.deviceid, newState)
+
+	delay = 0
+	if (req.params.delay) {
+		delay = Math.min(Math.max(parseInt(req.params.delay), 0), 600)*1000
+	}
+	console.log(delay)
+
+	setTimeout(() => {
+		ewlConnect().setDevicePowerState(config.deviceid, newState)
+	}, delay)
+
 	res.send("success")
 })
